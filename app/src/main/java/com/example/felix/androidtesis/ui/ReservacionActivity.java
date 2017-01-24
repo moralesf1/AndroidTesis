f1b78e7668d05846fbec239bf2ecee61925ed91a
Daniel Pargas <danielpargas14@gmail.com>
2017-01-24 16:54:10 -0400

    Cambios varios

--- app/src/main/java/com/example/felix/androidtesis/ui/ReservacionActivity.java
+++ app/src/main/java/com/example/felix/androidtesis/ui/ReservacionActivity.java
@@ -1,8 +1,14 @@
 package com.example.felix.androidtesis.ui;
 
 import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.felix.androidtesis.R;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
@@ -11,9 +17,18 @@ import java.util.Calendar;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class ReservacionActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class ReservacionActivity extends AppCompatActivity {
 
     private Context mContext;
+
+    private FloatingActionButton fabFechaInicio;
+    private FloatingActionButton fabFechaFin;
+
+    private TextView tvFechaInicio;
+    private TextView tvFechaFin;
+
+    private String mFechaInicio;
+    private String mFechaFin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +37,62 @@ public class ReservacionActivity extends AppCompatActivity implements DatePicker
 
         mContext = this;
 
-        Calendar now = Calendar.getInstance();
-        DatePickerDialog dpd = DatePickerDialog.newInstance(
-                this,
-                now.get(Calendar.YEAR),
-                now.get(Calendar.MONTH),
-                now.get(Calendar.DAY_OF_MONTH)
-        );
-        dpd.show(getFragmentManager(), "Datepickerdialog");
-    }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    @Override
-    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setTitle("");
+            toolbar.setTitle("");
+        }
+
+
+        fabFechaInicio = (FloatingActionButton) findViewById(R.id.fab_fecha_inicio);
+        fabFechaFin = (FloatingActionButton) findViewById(R.id.fab_fecha_fin);
+
+        tvFechaInicio = (TextView) findViewById(R.id.tv_fecha_inicio);
+        tvFechaFin = (TextView) findViewById(R.id.tv_fecha_fin);
+
+        fabFechaInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                                mFechaInicio = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                                tvFechaInicio.setText(mFechaInicio);
+                            }
+                        },
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+            }
+        });
+
+        fabFechaFin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                                mFechaFin = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                                tvFechaFin.setText(mFechaFin);
+                            }
+                        },
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+            }
+        });
 
     }
 
@@ -42,4 +101,9 @@ public class ReservacionActivity extends AppCompatActivity implements DatePicker
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_reservacion, menu);
+        return true;
+    }
 }
