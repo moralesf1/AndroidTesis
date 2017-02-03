a2e2de58cc7b82390f1b013bf035fa465be19a50
Daniel Pargas <danielpargas14@gmail.com>
2017-02-03 14:03:09 -0400

    Agregado modelo habitacion

---
+++ app/src/main/java/com/example/felix/androidtesis/modelo/Habitacion.java
@@ -0,0 +1,161 @@
+package com.example.felix.androidtesis.modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by saleventa on 2/3/17.
+ */
+
+public class Habitacion implements Serializable {
+
+    private int id;
+    private int user_id;
+
+    private String hotel;
+    private String banner;
+    private String keywords;
+    private String descripcion;
+
+    private int pais_id;
+    private int estado_id;
+    private int ciudad_id;
+
+    private String deleted_at;
+    private String created_at;
+    private String updated_at;
+
+    private Pais pais;
+    private Estado estado;
+    private Ciudad ciudad;
+
+    private ArrayList<Foto> fotos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public String getBanner() {
+        return banner;
+    }
+
+    public void setBanner(String banner) {
+        this.banner = banner;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPais_id() {
+        return pais_id;
+    }
+
+    public void setPais_id(int pais_id) {
+        this.pais_id = pais_id;
+    }
+
+    public int getEstado_id() {
+        return estado_id;
+    }
+
+    public void setEstado_id(int estado_id) {
+        this.estado_id = estado_id;
+    }
+
+    public int getCiudad_id() {
+        return ciudad_id;
+    }
+
+    public void setCiudad_id(int ciudad_id) {
+        this.ciudad_id = ciudad_id;
+    }
+
+    public String getDeleted_at() {
+        return deleted_at;
+    }
+
+    public void setDeleted_at(String deleted_at) {
+        this.deleted_at = deleted_at;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public ArrayList<Foto> getFotos() {
+        return fotos;
+    }
+
+    public void setFotos(ArrayList<Foto> fotos) {
+        this.fotos = fotos;
+    }
+}
