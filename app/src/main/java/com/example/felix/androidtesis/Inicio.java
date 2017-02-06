91cfd92f805d4354766f40492ade8ab3871edc91
Daniel Pargas <danielpargas14@gmail.com>
2017-02-06 16:51:54 -0400

    Cambios varios en diseno

--- app/src/main/java/com/example/felix/androidtesis/Inicio.java
+++ app/src/main/java/com/example/felix/androidtesis/Inicio.java
@@ -175,9 +175,9 @@ public class Inicio extends AppCompatActivity
         } else if (id == R.id.nav_gallery) {
             mostrarListaHoteles();
             mostrarTitulo("Hoteles");
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.sesion) {
             Sesion sesion = new Sesion();
